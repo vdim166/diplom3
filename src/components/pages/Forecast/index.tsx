c740b313fd14deefce5eff0c8436dae94318ae17
vadim <166vadim.lukanov166@gmail.com>
2025-03-24 22:31:31 +0300

    forecast page skeleton done

--- src/components/pages/Forecast/index.tsx
+++ src/components/pages/Forecast/index.tsx
@@ -1,3 +1,14 @@
+import cls from "./styles.module.scss";
+
 export const Forecast = () => {
-  return <div>Forecast</div>;
+  return (
+    <div className={cls.main}>
+      <div className={cls.window}>
+        <p>NAME</p>
+      </div>
+      <div className={cls.buttons}>
+        <button className={cls.generate}>Создать прогноз</button>
+      </div>
+    </div>
+  );
 };
